4d0fb1f2a88b98663455602d584c30b1006e7c70
Chris Brown and Onsi Fakhouri <pair+cbrown+onsi@pivotallabs.com>
2013-08-23 17:48:35 -0700

    add a test for parsing a malformed line

--- cartridge_parser_test.go
+++ cartridge_parser_test.go
@@ -13,31 +13,45 @@ var _ = Describe("Parsing cartridges", func() {
 		err          error
 	)
 
-	BeforeEach(func() {
-		cartridge = bytes.NewBuffer([]byte(`
+	Context("with a well-formed cartridge", func() {
+		BeforeEach(func() {
+			cartridge = bytes.NewBuffer([]byte(`
 foo.com/bar master
 
 # i'm a pretty comment
 fizz.buzz/foo last:1`))
 
-		dependencies, err = ParseDependencies(cartridge)
+			dependencies, err = ParseDependencies(cartridge)
+		})
+
+		It("parses without error", func() {
+			Expect(err).NotTo(HaveOccured())
+		})
+
+		It("has the correct number of dependencies", func() {
+			Expect(dependencies).To(HaveLen(2))
+
+			Expect(dependencies[0]).To(Equal(Dependency{
+				Path:    "foo.com/bar",
+				Version: "master",
+			}))
+
+			Expect(dependencies[1]).To(Equal(Dependency{
+				Path:    "fizz.buzz/foo",
+				Version: "last:1",
+			}))
+		})
 	})
 
-	It("parses without error", func() {
-		Expect(err).NotTo(HaveOccured())
-	})
+	Context("when the cartridge has a mal-formed line", func() {
+		BeforeEach(func() {
+			cartridge = bytes.NewBuffer([]byte(`foo.com/bar`))
 
-	It("has the correct number of dependencies", func() {
-		Expect(dependencies).To(HaveLen(2))
+			_, err = ParseDependencies(cartridge)
+		})
 
-		Expect(dependencies[0]).To(Equal(Dependency{
-			Path:    "foo.com/bar",
-			Version: "master",
-		}))
-
-		Expect(dependencies[1]).To(Equal(Dependency{
-			Path:    "fizz.buzz/foo",
-			Version: "last:1",
-		}))
+		It("returns an error", func() {
+			Expect(err.Error()).To(Equal("malformed line: foo.com/bar"))
+		})
 	})
 })
