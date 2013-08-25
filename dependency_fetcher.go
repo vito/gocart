d1ee365376f260807c8950c9f68fc195a33218c6
Chris Brown <xoebus@xoeb.us>
2013-08-24 22:05:04 -0700

    clean up dependency fetcher

--- dependency_fetcher.go
+++ dependency_fetcher.go
@@ -17,8 +17,12 @@ func NewDependencyFetcher(runner CommandRunner) *DependencyFetcher {
 
 func (fetcher *DependencyFetcher) Fetch(dependency Dependency) error {
 	cmd := exec.Command("go", "get", "-u", "-d", "-v", dependency.Path)
+	fetcher.redirectIO(cmd)
+	return fetcher.commandRunner.Run(cmd)
+}
+
+func (fetcher *DependencyFetcher) redirectIO(cmd *exec.Cmd) {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Stdin = os.Stdin
-	return fetcher.commandRunner.Run(cmd)
 }
